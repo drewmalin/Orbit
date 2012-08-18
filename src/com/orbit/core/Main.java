4fb14688e2dfbb5d52f7488bb14212bb9cf8d6fb
Drew Malin <drewmalin@gmail.com>
2012-08-18 11:24:30 -0700

    Sample opengl window

--- src/com/orbit/core/Main.java
+++ src/com/orbit/core/Main.java
@@ -1,7 +1,33 @@
 package com.orbit.core;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.Display;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		Main displayExample = new Main();
+		displayExample.start();
+	}
+	
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// init OpenGL here
+		
+		while (!Display.isCloseRequested()) {
+			
+			// render OpenGL here
+			
+			Display.update();
+		}
+		
+		Display.destroy();
 	}
 }
